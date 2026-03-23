8c922d9024a9aa49aae96c5b62e5bc27902bdb8b
isaacudofia <isaacudofia0@gmail.com>
2026-03-23 23:25:00 +0100

    Final Push for today

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
 import Hero from "@/components/Hero";
 import Works from "@/components/Works";
-import Services from "@/components/Services";
-import Experience from "@/components/Experience";
-import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Works />
-      <Services />
-      <Experience />
-      <Contact />
       {/* You can add your project grid below this later */}
     </main>
   );
